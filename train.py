66fe76f53a3ac51224f78f7186eaadec82ffc1a9
Patrick McGrath <ptmcgrat@gmail.com>
2019-07-08 09:55:15 -0400

    Test

--- train.py
+++ train.py
@@ -21,7 +21,7 @@ def train_epoch(epoch, data_loader, model, criterion, optimizer, opt,
     end_time = time.time()
     for i, (inputs, targets, paths) in enumerate(data_loader):
         data_time.update(time.time() - end_time)
-
+        pdb.set_trace()
         if not opt.no_cuda:
             targets = targets.cuda(async=True)
         inputs = Variable(inputs)
